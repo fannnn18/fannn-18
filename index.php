30b1e34964b0246febd42f2dcd8fc869fbdc735f
fannnn18 <159591601+fannnn18@users.noreply.github.com>
2024-08-21 15:46:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 <html lang="en">
 <head>
   <title>Special For My Lia</title>
-  <style>
-    body {
-      background: grey;
-    }
-  </style>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
